6ea8294882514d6c7cae7c49ff0275d4d4e41462
Jean SIMARD <woshilapin@tuziwo.info>
2016-10-27 10:58:43 -0400

    Remove nesting 'new Promise' when possible

--- src/utils.js
+++ src/utils.js
@@ -10,32 +10,30 @@ var login = function login(tries) {
 	if (tries === undefined || typeof tries !== 'number') {
 		tries = 3;
 	}
-	return new Promise(function(resolve, reject) {
-		var credentials = {};
-		configure.get('username', 'shell', 'login? ')
-		.then(function(username) {
-			credentials.username = username;
-			return configure.get('password', 'shell', 'password? ');
-		})
-		.then(function(password) {
-			credentials.password = password;
-			return cgapi.login(credentials.username, credentials.password);
-		})
-		.then(function(res) {
-			if (!res.error) {
-				resolve(res);
+	var credentials = {};
+	return configure.get('username', 'shell', 'login? ')
+	.then(function(username) {
+		credentials.username = username;
+		return configure.get('password', 'shell', 'password? ');
+	})
+	.then(function(password) {
+		credentials.password = password;
+		return cgapi.login(credentials.username, credentials.password);
+	})
+	.then(function(res) {
+		if (!res.error) {
+			return Promise.resolve(res);
+		} else {
+			tries -= 1;
+			if (tries > 0) {
+				console.warn(`Unable to login (${tries} ${tries>1?'tries':'try'} remaining).`);
+				configure.forget('username');
+				configure.forget('password');
+				return Promise.resolve(login(tries));
 			} else {
-				tries -= 1;
-				if (tries > 0) {
-					console.warn(`Unable to login (${tries} ${tries>1?'tries':'try'} remaining).`);
-					configure.forget('username');
-					configure.forget('password');
-					resolve(login(tries));
-				} else {
-					reject(new Error('Unable to login.'))
-				}
+				return Promise.reject(new Error('Unable to login.'))
 			}
-		});
+		}
 	});
 };
 
