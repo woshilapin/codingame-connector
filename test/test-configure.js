20189499d301c2a5d6cce184ebb4d81240e5547a
Jean SIMARD <woshilapin@tuziwo.info>
2016-11-01 15:19:30 -0400

    Use Chai and Chai As Promised as assertion framework

--- test/test-configure.js
+++ test/test-configure.js
@@ -1,16 +1,16 @@
-import assert from 'assert';
+import chai from 'chai';
+import chaiaspromised from 'chai-as-promised';
 import configure from '../src/configure.js';
+
+let expect = chai.expect;
+chai.use(chaiaspromised);
 
 describe(`[module] configure`, function() {
 	describe(`[method] load`, function() {
-		it(`should return options if path is incorrect and options is defined`, function(done) {
+		it(`should return options if path is incorrect and options is defined`, function() {
 			let options = { "some": true };
-			configure.load(undefined, options).then(function(conf) {
-				assert.equal(conf, options);
-				done();
-			}, function(error) {
-				done(error);
-			})
+			let conf = configure.load(undefined, options);
+			return expect(conf).to.eventually.be.deep.equal(options);
 		});
 	});
 });
