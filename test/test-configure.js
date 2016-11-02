5e51e6506e69b1b5d426251d6dbb4b78fd7c817a
Jean SIMARD <woshilapin@tuziwo.info>
2016-11-02 14:14:19 -0400

    Use mock-fs for 'fs' module mocking

--- test/test-configure.js
+++ test/test-configure.js
@@ -1,5 +1,7 @@
 import chai from 'chai';
 import chaiaspromised from 'chai-as-promised';
+import mockfs from 'mock-fs';
+
 import configure from '../src/configure.js';
 
 let expect = chai.expect;
@@ -7,10 +9,32 @@ chai.use(chaiaspromised);
 
 describe(`[module] configure`, function() {
 	describe(`[method] load`, function() {
+		let defaultconfpath = `.codingamerc`;
+		let defaultconf = {
+			"username": `me`,
+			"password": `somepass`,
+			"exercise": `5711567e959cf54dd2dd79c1b4c259560d6ba46`,
+			"tests": [1, 2],
+			"language": `Python`,
+			"bundle": `bundle.py`
+		};
+		before(function () {
+			mockfs({
+				[defaultconfpath]: JSON.stringify(defaultconf)
+			});
+		});
+		after(function () {
+			mockfs.restore();
+		});
+
+		it(`should return the content of file parsed as JSON`, function() {
+			let conf = configure.load(defaultconfpath);
+			return expect(conf).to.eventually.be.deep.equal(defaultconf);
+		});
+
 		it(`should return options if path is incorrect and options is defined`, function() {
-			let options = { "some": true };
-			let conf = configure.load(undefined, options);
-			return expect(conf).to.eventually.be.deep.equal(options);
+			let conf = configure.load(undefined, defaultconf);
+			return expect(conf).to.eventually.be.deep.equal(defaultconf);
 		});
 	});
 });
